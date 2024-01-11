32c6c4561b77095e8e6cf21d7b14ebe99b113e16
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2024-01-11 14:16:05 +0000

    Fixed typo.

--- model_fit_SA.py
+++ model_fit_SA.py
@@ -2,7 +2,7 @@ from utils import *
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas import read_csv
-from scipy.optimize import minimize
+from scipy.optimize import anneal
 from model_fit_simple import *
 
 if __name__ == '__main__':
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     bound_high= [ 0.06, 30, 9, 0.0015, -20, -3, 5e-3, -2.0, 2, 8e-3]
 
     resfile= open("fit_trajectory_SA.txt","w")
-    res_sa= (fit_fun,x0, args=(trg_sdf,the_conc,net,p,resfile),lower=bound_low,upper=bound_high,full_output=True,maxeval=1000000)
+    res_sa= anneal(fit_fun,x0, args=(trg_sdf,the_conc,net,p,resfile),lower=bound_low,upper=bound_high,full_output=True,maxeval=1000000)
 
     print(res_sa)
     resfile.close()
